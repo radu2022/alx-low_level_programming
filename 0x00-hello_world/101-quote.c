0ac4155a32bb24f133810affba662237d4b6fb2e
seid hamid <seidhamid200@gmail.com>
2022-06-28 15:53:07 -0700

    betty fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * main - main function
- *
  * Return: end program
  */
 int main(void)
 {
-  char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-  fprintf(stderr, "%s\n", n);
+fprintf(stderr, "%s\n", n);
 
-  return (1);
+return (1);
 }
