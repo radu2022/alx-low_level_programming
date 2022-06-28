fd3e66e432a352aa39ffcb9e5662344076b6633f
seid hamid <seidhamid200@gmail.com>
2022-06-28 14:48:53 -0700

    unix simplicity advanced still try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Seid Hamid
+ */
+
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always 0 (Success)
+ * Return: Always 1.
  */
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+  write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 
   return (1);
 }
