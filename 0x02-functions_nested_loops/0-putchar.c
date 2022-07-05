4d9d2cdcfd10d6cd11e560f49b383d4f1e733f1a
seid hamid <seidhamid200@gmail.com>
2022-07-05 10:07:11 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+  _putchar('_putchar');
+  _putchar('\n');
+
+  return (0);
+}
