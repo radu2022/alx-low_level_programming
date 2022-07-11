383fbb57ed41c371f77f1fc30b300dc9ec9059cc
seid hamid <seidhamid200@gmail.com>
2022-07-11 12:59:28 -0700

    print n element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ * @a: pointer int type
+ * @n: is a int variable
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+
+int i, inte;
+for (i = 0; i < n; i++)
+{
+inte = *(a + i);
+printf("%d", inte);
+if (i != n - 1)
+printf(", ");
+}
+printf("\n");
+
+}
