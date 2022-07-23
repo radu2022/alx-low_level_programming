aaddd0c1511b86b77334015c4e3e1b82d5e7de96
seid hamid <seidhamid200@gmail.com>
2022-07-23 13:29:02 -0700

    isspace strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,6 @@
 /**
  * _isspace - check if a character is whitespace
  * @c: the character to check
- *
  * Return: 1 is c is a whitespace character, otherwise 0
  */
 int _isspace(int c)
@@ -16,7 +15,6 @@ return (0);
 /**
  * strtow - split a string into words
  * @str: a pointer to the string to split
- *
  * Return: NULL if memory allocation fails or if str is NULL or empty (""),
  * otherwise return a pointer to the array of words terminated by a NULL
  */
@@ -62,5 +60,4 @@ words[w][c] = '\0';
 } while (++w, *pos);
 words[w] = NULL;
 return (words);
-
 }
