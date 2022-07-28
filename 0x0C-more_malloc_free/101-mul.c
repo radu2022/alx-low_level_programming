6ee63151d7a8791db2a709487f4e6a61864a3bd9
seid hamid <seidhamid200@gmail.com>
2022-07-28 12:08:12 -0700

    101 mul fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,210 +1,195 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * add - addition of parcial products
- * @p: parcial product
- * @mg: big malloc
- * @i: size of big malloc - 1
- * @tp: size of parcial product - 1
+/*
+ * File: 101-mul.c
+ * Auth: Seid H Muhammed
  */
-void add(char *p, char *mg, int i, int tp)
+
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+/**
+ * find_len - Finds the length of a string.
+ * @str: The string to be measured.
+ * Return: The length of the string.
+ */
+int find_len(char *str)
 {
 
-int pos = tp, a = 0, b = 0, car = 0, res = 0;
-int cont = tp, aux = 0;
-for (; cont >=  0; cont--, i--, pos--)
-{
-a = *(p + pos) - '0';
-b = *(mg + i) - '0';
-res = (a + b)+car;
-car = res / 10;
-res = (res % 10) + '0';
-*(mg + i) = res;
-}
-res = 0;
-for (aux = i - 1; aux >= 0; aux--)
-{
-a = *(mg + aux) - '0';
-a += a + car;
-car = a / 10;
-a = (a % 10) + '0';
-*(mg + aux) = a;
-}
+int len = 0;
+while (*str++)
+len++;
+return (len);
 }
 /**
- * setiar - fill the pointer with 0
- * @s: pointer
- * @a: size of pointer
+ * create_xarray - Creates an array of chars and initializes it with
+ *                 the character 'x'. Adds a terminating null byte.
+ * @size: The size of the array to be initialized.
+ * Description: If there is insufficient space, the
+ *              function exits with a status of 98.
+ * Return: A pointer to the array.
  */
-void setiar(char *s, int a)
+char *create_xarray(int size)
 {
-int i;
-for (i = 0; i < a; i++)
-s[i] = '0';
+char *array;
+int index;
+array = malloc(sizeof(char) * size);
+if (array == NULL)
+exit(98);
+for (index = 0; index < (size - 1); index++)
+array[index] = 'x';
+array[index] = '\0';
+return (array);
 }
 /**
- * parcial - give the parcial malloc
- * @a: size one
- * Return: parcial malloc
+ * iterate_zeroes - Iterates through a string of numbers containing
+ *                  leading zeroes until it hits a non-zero number.
+ * @str: The string of numbers to be iterate through.
+ * Return: A pointer to the next non-zero element.
  */
-char *parcial(int a)
+char *iterate_zeroes(char *str)
 {
-int i;
-char *p;
-p = malloc(a);
-if (!p)
-return (NULL);
-for (i = 0; i < a; i++)
-p[i] = '0';
-return (p);
+while (*str && *str == '0')
+str++;
+return (str);
 }
 /**
- * grande - give big malloc
- * @a: size one
- * @b: size two
- * Return: malloc
+ * get_digit - Converts a digit character to a corresponding int.
+ * @c: The character to be converted.
+ * Description: If c is a non-digit, the function
+ *              exits with a status of 98.
+ * Return: The converted int.
  */
-char *grande(int a, int b)
+int get_digit(char c)
 {
-int i;
-char *p;
-p = malloc(a + b);
-if (!p)
-return (NULL);
-for (i = 0; i < a + b; i++)
-p[i] = '0';
-return (p);
-}
-/**
- * tama - give the size
- * @s: string of numbers
- * Return: size
- */
-int tama(char *s)
+int digit = c - '0';
+if (digit < 0 || digit > 9)
 {
-int i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-return (i);
-}
-/**
- * Perror - print Error with putchar.
- * Return: Always 0.
- */
-void Perror(void)
-{
-_putchar('E');
-_putchar('r');
-_putchar('r');
-_putchar('o');
-_putchar('r');
-_putchar('\n');
+printf("Error\n");
 exit(98);
 }
+return (digit);
+}
 /**
- * numero - check if strings have only numbers.
- * @s: string 1
- * @a: string 2
- * Return: Always 1 if not or 0 if only has number.
+ * get_prod - Multiplies a string of numbers by a single digit.
+ * @prod: The buffer to store the result.
+ * @mult: The string of numbers.
+ * @digit: The single digit.
+ * @zeroes: The necessary number of leading zeroes.
+ * Description: If mult contains a non-digit, the function
+ *              exits with a status value of 98.
  */
-void numero(char *s, char *a)
+void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
-int i = 0, aux = 0;
-while (s[i] != '\0')
+int mult_len, num, tens = 0;
+mult_len = find_len(mult) - 1;
+mult += mult_len;
+while (*prod)
 {
-if (!(s[i] >= '0' && s[i] <= '9'))
-{
-aux = 1;
-break;
+*prod = 'x';
+prod++;
 }
-i++;
-}
-i = 0;
-while (a[i] != '\0')
+prod--;
+while (zeroes--)
 {
-if (!(a[i] >= '0' && a[i] <= '9'))
+*prod = '0';
+prod--;
+}
+for (; mult_len >= 0; mult_len--, mult--, prod--)
 {
-aux = 1;
-break;
-}
-i++;
-}
-if (aux == 1)
-Perror();
-if (*s == '0' || *a == '0')
+if (*mult < '0' || *mult > '9')
 {
-_putchar('0');
-_putchar('\n');
-exit(0);
+printf("Error\n");
+exit(98);
 }
-
+num = (*mult - '0') * digit;
+num += tens;
+*prod = (num % 10) + '0';
+tens = num / 10;
+}
+if (tens)
+*prod = (tens % 10) + '0';
 }
 /**
- * impresion - print the pointer
- * @s: string.
- * @a: size of string
+ * add_nums - Adds the numbers stored in two strings.
+ * @final_prod: The buffer storing the running final product.
+ * @next_prod: The next product to be added.
+ * @next_len: The length of next_prod.
  */
-void impresion(char *s, int a)
+void add_nums(char *final_prod, char *next_prod, int next_len)
 {
-
-int i;
-for (i = 0; i < a; i++)
+int num, tens = 0;
+while (*(final_prod + 1))
+final_prod++;
+while (*(next_prod + 1))
+next_prod++;
+for (; *final_prod != 'x'; final_prod--)
 {
-if (!(s[0] == '0' && i == 0))
-_putchar(s[i]);
+num = (*final_prod - '0') + (*next_prod - '0');
+num += tens;
+*final_prod = (num % 10) + '0';
+tens = num / 10;
+next_prod--;
+next_len--;
 }
-_putchar('\n');
-
+for (; next_len >= 0 && *next_prod != 'x'; next_len--)
+{
+num = (*next_prod - '0');
+num += tens;
+*final_prod = (num % 10) + '0';
+tens = num / 10;
+final_prod--;
+next_prod--;
+}
+if (tens)
+*final_prod = (tens % 10) + '0';
 }
 /**
- * main - check the code for Holberton School students.
- * @argv: array of pointers store the arguments
- * @argc: quantity of arguments
+ * main - Multiplies two positive numbers.
+ * @argv: The number of arguments passed to the program.
+ * @argc: An array of pointers to the arguments.
+ * Description: If the number of arguments is incorrect or one number
+ *              contains non-digits, the function exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-
-int t1, t2, t3, a = 0, b = 0, j = 0, aux = 0, car, g, ind;
-char *mg, *p, *size1, *size2;
+char *final_prod, *next_prod;
+int size, index, digit, zeroes = 0;
 if (argc != 3)
-Perror();
-else
-{ numero(argv[1], argv[2]);
-t1 = tama(argv[1]);
-t2 = tama(argv[2]);
-ind = t1 + t2;
-mg = grande(t1, t2);
-if (t1 >= t2)
-{ size1 = argv[1];
-size2 = argv[2];
-b = t1 - 1;
-a = t2 - 1;
-t3 = t1 + 1; }
-else
-{ size1 = argv[2];
-size2 = argv[1];
-b = t2 - 1;
-a = t1 - 1;
-t3 = t2 + 1; }
-g = b;
-p = parcial(t3);
-j = b + 1;
-for (; a >= 0; a--, ind--)
-{ car = 0;
-b = g;
-j = g + 1;
-setiar(p, t3);
-for (; b >= 0; b--, j--)
-{ aux = ((size2[a] - '0') * (size1[b] - '0'));
-aux = aux + car;
-p[j] = ((aux % 10) + '0');
-car = aux / 10; }
-p[j] = (car + '0');
-add(p, mg, (ind - 1), (t3 - 1)); }
-impresion(mg, (t1 + t2)); }
+{
+printf("Error\n");
+exit(98);
+}
+if (*(argv[1]) == '0'argv[1] = iterate_zeroes(argv[1]);
+if (*(argv[2]) == '0')
+argv[2] = iterate_zeroes(argv[2]);
+if (*(argv[1]) == '\0' || *(argv[2]) == '\0')
+{
+printf("0\n");
 return (0);
+}
+size = find_len(argv[1]) + find_len(argv[2]);
+final_prod = create_xarray(size + 1);
+next_prod = create_xarray(size + 1);
+for (index = find_len(argv[2]) - 1; index >= 0; index--)
+{
+digit = get_digit(*(argv[2] + index));
+get_prod(next_prod, argv[1], digit, zeroes++);
+add_nums(final_prod, next_prod, size - 1);
+}
+for (index = 0; final_prod[index]; index++)
+{
+if (final_prod[index] != 'x')
+putchar(final_prod[index]);
+}
+putchar('\n');
+free(next_prod);
+free(final_prod);
+return (0);
+
 }
