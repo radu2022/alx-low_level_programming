02b5a6faa1cdc86d85c0482ecdf804aad26c0a3e
seid hamid <seidhamid200@gmail.com>
2022-08-01 12:03:48 -0700

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-
-#define ABS(x) ((x) < (0) ? (-x) : (x))
-
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 #endif
