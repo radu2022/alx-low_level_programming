855e546d4dddabf51c03711525c3d479a18896f5
seid hamid <seidhamid200@gmail.com>
2022-08-11 06:01:13 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,18 @@
 #include "lists.h"
+
 /**
- * list_len - counts the length of the list
- * @h: the list
- * Return: the # of slots
+ * list_len - returns the number of node in a list
+ * @h: a given lsit head
+ *
+ * Return: a length of the list
  */
 size_t list_len(const list_t *h)
 {
-
-const list_t *z_list = h;
-int count = 0;
-if (h == NULL)
-return (0);
-while (z_list != NULL)
+size_t length = 0;
+while (h != 0)
 {
-z_list = z_list->next;
-count++;
+length++;
+h = h->next;
 }
-return (count);
-
+return (length);
 }
