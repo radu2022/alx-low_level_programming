dbd7f0a78a14e8f884cdc9f967bdce9332b5d3c4
radu2022 <seid.hamid2@gmail.com>
2022-10-03 09:59:47 -0700

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/radu2022/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+wget -0 /tmp/win.so https://github.com/radu2022/alx-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
 export LD_PRELOAD=/tmp/win.so
