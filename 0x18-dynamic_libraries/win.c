bde961d6d06ab17e4beab83759a0710eecab8083
radu2022 <seid.hamid2@gmail.com>
2022-10-04 09:07:03 -0700

    libwin

---
+++ 0x18-dynamic_libraries/win.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int printf (const char *format, ...)
+{
+/* Our code goes here
+* We can use write for printing function
+* */
+write (1, "9 8 10 24 75 - 9\n", 17);
+write (1, "Congratulations, you win the jackpot!\n", 38);
+exit (EXIT_SUCCESS);
+}
