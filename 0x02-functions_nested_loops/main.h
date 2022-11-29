bfa9efcea6832cf86a0ab865c3602461ce98f146
radu2022 <seidhamid200@gmail.com>
2022-11-29 11:15:21 -0800

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- *File:main.h
- *Author:Seid H
- *Desc:header file containing functions declaration
+/*
+ * Header file for all prototypes
+ * used in 0x02-functions_nested_loops
  */
 
 void print_alphabet(void);
 void jack_bauer(void);
 void print_alphabet_x10(void);
-void times_table();
+void times_table(void);
 void print_to_98(int);
 void print_times_table(int);
 int _putchar(char);
@@ -18,3 +19,4 @@ int print_sign(int n);
 int _abs(int);
 int add(int n, int m);
 int print_last_digit(int n);
+#endif
